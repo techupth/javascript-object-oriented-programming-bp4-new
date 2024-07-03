b93fbb0268d835c3d3467f2bd118a96097af76bb
Wasin <wasin1026@gmail.com>
2024-07-03 22:22:10 +0700

    ex1

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,27 @@
-class EmailNotification { }
+class EmailNotification { 
+    constructor(notificationId,createdTime,content,receiver){
+        this.notificationId=notificationId;
+        this.createdTime=createdTime;
+        this.content=content;
+        this.receiver=receiver;
+    }
+    send(){
+        return console.log("Notification has been sent to "+this.receiver);
 
-class SMSNotification { }
+    }
+}
+
+class SMSNotification extends EmailNotification {
+    constructor(notificationId,createdTime,content,phoneNumber){
+        super(notificationId,createdTime,content);
+        this.phoneNumber=phoneNumber;
+    }
+    send() {
+        return console.log("Notification has been sent to "+this.phoneNumber);
+ }
+}
+
+const email = new EmailNotification("email",10.00,"Hello world","wasin@gmail.com");
+const phoneNumber = new EmailNotification("email",10.00,"Hello world","0880768323");
+email.send();
+phoneNumber.send();
